214f1fdcde2d6f104f3c5f0c0ca4242c0ae730b6
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-09-14 08:13:19 +0900

    updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,29 +3,29 @@ import image from "../assets/image.jpg";
 
 const Card = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-zinc-700">
-      <div className="bg-zinc-800 p-2 mx-6 rounded-2xl">
-        <div className="flex flex-col md:flex-row rounded-l-xl">
+    <div className="bg-zinc-700 h-screen flex items-center justify-center">
+      <div className="p-2 mx-6 rounded-2xl bg-zinc-800">
+        <div className="flex flex-col md:flex-row">
           <img
             src={image}
             alt=""
-            className="object-fit rounded-xl h-80 md:h-64 md:rounded-l-xl md:rounded-r-none transform hover:scale-105 hover:rounded-xl duration-200"
-          />
+            className="rounded-xl h-80 md:h-64 md:rounded-l-64 md:rounded-r-none transform hover:scale-105 duration-200"
+          ></img>
           <div className="p-6 md:p-12">
-            <h2 className="font-serif text-xl font-medium text-center text-white md:text-left">
+            <h2 className="text-white font-serif text-center md:text-left">
               Get diet and fitness tips in your inbox
             </h2>
-            <p className="max-w-xs my-4 text-xs leading-5 tracking-wide text-center text-white md:text-left">
+            <p className="max-w-xs text-xs text-white my-4 leading-5 tracking-wide text-center md:text-left">
               Eat better and exercise better. Sign up for the Diet & Fitness
               newsletter.
             </p>
-            <div className="flex flex-col mt-5 space-y-4 md:space-x-3 md:flex-row md:space-y-0">
+            <div className="flex flex-col mt-5 md:flex-row space-y-4 md:space-x-3 md:space-y-0">
               <input
                 type="text"
-                placeholder="Enter you email address"
-                className="p-2 px-4 text-center text-white bg-zinc-800 border border-zinc-600 placeholder:text-xs placeholder:text-center md:text-left placeholder:md:text-left focus:outline-none"
+                placeholder="Enter your email address"
+                className="px-2 py-4 rounded-md focus:outline-none bg-zinx-800 border-zinc-600 placeholder:text-center md:placeholder:text-left"
               ></input>
-              <button className="px-5 py-3 text-xs rounded-md text-zinc-800 bg-lime-500 hover:bg-lime-700 hover:text-white duration-500">
+              <button className="py-4 px-5 rounded-md bg-lime-500 hover:bg-lime-700 hover:text-white duration-200">
                 Subscribe
               </button>
             </div>
